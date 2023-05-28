702005cab558a7b81ffe725622ed529fa5817882
Denis <dennisjose.us@gmail.com>
2023-05-27 22:01:25 -0600

    Initial commit nav complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Nav from "./components/Nav"
+import About from "./components/About"
+// Importing Components
+function App() {
+	return (
+		<div>
+			<Nav />
+			<About />
+		</div>
+	)
+}
+
+export default App
