65ce1bf5523f9e12f62662a989c9301ed7d22d2f
Denis <dennisjose.us@gmail.com>
2023-05-29 15:38:10 -0600

    Navbar new version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from "react"
-import Nav from "./components/Nav"
-import About from "./components/About"
-import Home from "./components/Home"
+import React from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Home from "./components/Home";
 // Importing Components
 function App() {
-	return (
-		<div>
-			<Nav />
-			<Home />
-			<About />
-		</div>
-	)
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 }
 
-export default App
+export default App;
